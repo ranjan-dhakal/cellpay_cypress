a2842b1659ab7acc0e312d7c08d7851d1311db80
ranjan-dhakal <ranjan.cellcom.net@gmail.com>
2023-04-12 12:24:57 +0545

    government services using wrap functionality

--- cypress/e2e/cellpay/13_government.cy.js
+++ cypress/e2e/cellpay/13_government.cy.js
@@ -3,16 +3,42 @@ const { login } = require("./0_login.cy");
 
 beforeEach(() => {
     login();
-
     cy.wait(3000)
-
     cy.get("[href='/dashboard/governmentPayment']").eq(0)
         .click();
-   
-});
+   });
 
-describe('Governmemt Payment', () => {
-    it.only('From WALLET', () => {
-        
-    });
-});
+describe('My Cypress Test Suite', () => {
+    const testCases = [    
+        { title: ' CRO ', alt: 'cro', ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+        { title: ' Department Of Customs ', alt: 'departmentOfCustoms', ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+        { title: ' Government Revenue (FCGO) ', alt: "governmentRevenue(FCGO)", ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+        { title: ' IRD ', alt: 'ird', ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+        { title: ' LokSewa ', alt: 'loksewa', ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+        { title: ' Passport ', alt: 'passport', ebpget: `[name='ebpNumber']`, enumber:'12345', amountget: `[name='amount']`, amount:'100'},
+     ]
+  
+    it.only('it should navigate to six multiple government services', () => {
+      cy.wrap(testCases).each((testCase) => {
+        cy.wait(3000)
+        cy.get(`[alt="${testCase.alt}"]`)
+            .click()
+        cy.get('.card-header')
+            .eq(1)
+            .should('have.text',testCase.title)
+            cy.scrollTo(0,0)
+        cy.get(testCase.ebpget)
+            .type(testCase.enumber)
+            cy.scrollTo(0,0)
+        cy.get(testCase.amountget)
+            .type(testCase.amount)
+        cy.get('[type="submit"]')
+            .click()
+        cy.get('.has-error').should('have.text','Your request could not be completed at this moment, please try after sometime.')
+        cy.scrollTo(0,0)
+        cy.wait(3000)
+        cy.reload()
+        })
+    })
+
+  })
--- cypress/e2e/wrapexample.cy.js
+++ cypress/e2e/wrapexample.cy.js
@@ -3,20 +3,27 @@ const { login } = require("./0_login.cy");
 
 describe('My Cypress Test Suite', () => {
     const testCases = [    
-        { title: 'CellPay', url: 'https://app.cellpay.com.np/dashboard/governmentPayment' }
+        { title: 'CRO', href: 'loksewa' },
+        { title: 'Department Of Customs', href: 'https://app.cellpay.com.np/dashboard/governmentPayment' },
+        { title: 'Government Revenue (FCGO)', href: 'https://app.cellpay.com.np/dashboard/governmentPayment' },
+        { title: 'IRD', href: 'https://app.cellpay.com.np/dashboard/governmentPayment' },
+        { title: 'LokSewa', href: 'https://app.cellpay.com.np/dashboard/governmentPayment' },
+        { title: 'Passport', href: 'https://app.cellpay.com.np/dashboard/governmentPayment' }
      ]
   
     it.only('should navigate to multiple pages', () => {
       cy.wrap(testCases).each((testCase) => {
         login()
         cy.wait(3000)
-        cy.visit(testCase.url)
+        cy.get("[alt=testCase.href]")
+          .click()
         cy.title().should('contain', testCase.title)
-        cy.title().then(title => {
-            console.log(title)
-          }) // to print the title of the page into the console
         
       })
     })
   })
   
+
+  // cy.title().then(title => {
+  //   console.log(title)
+  // }) // to print the title of the page into the console
